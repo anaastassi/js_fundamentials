ecbb3fbb06ab59fb258dc1c287420cc2bf0b8136
anaastassi <101422994+anaastassi@users.noreply.github.com>
2023-04-13 20:51:19 +0300

    Add files via upload

---
+++ js/01_intro/main.js
@@ -0,0 +1,41 @@
+console.log("Olkhovyk");
+
+let variable1 = 1;
+let variable2 = 2;
+alert(`${variable1} , ${variable2}`);
+
+variable1 = variable2;
+alert(`${variable1} , ${variable2}`);
+
+let object = new Object();
+object.string = "string";
+object.number = 1;
+object.boolean = true;
+object.undefined;
+object.null = null;
+
+console.log((isAdult = window.confirm("Are you over 18 years old?")));
+
+let firstName = "Anastasiia";
+let lastName = "Olkhovyk";
+let group = "js fundamentials";
+let yearOfBirth = 1999;
+let isMarried = false;
+let color;
+let nationality = null;
+console.log(`Number: ${yearOfBirth}`);
+console.log(`Boolean: ${isMarried}`);
+console.log(`String: ${firstName}, ${lastName}, ${group}`);
+console.log(typeof color, typeof nationality);
+
+let login = prompt("Enter your login");
+let email = prompt("Enter your email");
+let password = prompt("Enter your password");
+alert(`Dear ${login}, your email is ${email}, your password is ${password}`);
+
+const hour = 60 * 60;
+const day = hour * 24;
+const month = day * 30;
+alert(`Seconds in an hour: ${hour}
+Seconds in a day: ${day}
+Seconds in a month: ${month}`);
