22ca9c1379786e70991e4c23c4a016ae0eecb20b
anaastassi <101422994+anaastassi@users.noreply.github.com>
2023-05-17 19:07:51 +0300

    Add files via upload

---
+++ jquery/09.js
@@ -0,0 +1,30 @@
+//1
+let $elements = $(".head");
+$elements.css("background-color", "green");
+let $innerElements = $elements.find(".inner");
+$innerElements.css("font-size", "35px");
+
+//2
+$("a[href^='https://']").attr("target", "_blank");
+
+//3
+let $divs = $("h3").next();
+for (let i = 0; i < $divs.length; i++) {
+  $divs[i].parentNode.insertBefore($divs[i], $divs[i].previousElementSibling);
+}
+
+//4
+let allCheckboxes = $("input:checkbox");
+allCheckboxes.change(function () {
+  let checkedCount = $(":checkbox:checked").length;
+  console.log(allCheckboxes);
+  if (checkedCount > 2) {
+    allCheckboxes.not(":checked").each(function () {
+      $(this)[0].disabled = true;
+    });
+  } else {
+    allCheckboxes.not(":checked").each(function () {
+      $(this)[0].disabled = false;
+    });
+  }
+});
